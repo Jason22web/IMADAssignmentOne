571782229c7870b8e775f71c428fc8b5f96576c5
Jason22web <ST10507997@myemeris.edu.za>
2026-03-29 18:54:17 +0200

    My first commit from android studio. I have done the majority of the layout, by adding Two button(s), ImageView(Added the drawable), Three TextView(s), and EditText constraining everything

---
+++ app/src/main/java/com/example/imadassessmentmarch30/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.imadassessmentmarch30
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        /*Declarations of variable for my app. this is the 1st major step I have taken into building the
+application for IMAD5112 assessment */
+
+//on my layout page I have to finish labeling and adding more to the interface
+
+        val enterTimeOfDayButton = findViewById<Button>(R.id.btnEnterTimeOfDay)
+        val resetButton = findViewById<Button>(R.id.ResetBtn)
+        val theSuggesting = findViewById<TextView>(R.id.SuggestingText)
+        val inputText = findViewById<EditText>(R.id.InputText)
+
+        enterTimeOfDayButton.setOnClickListener {
+            val input = enterTimeOfDayButton.text.toString()
+        }
+
+        }
+    }
+}
+
