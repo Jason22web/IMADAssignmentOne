2ded4ae8ec3749e53a376269c902301319a24e2c
Jason22web <ST10507997@myemeris.edu.za>
2026-03-29 21:54:47 +0200

    This is my Third commit I have finished the code required to run the app will run tests to see if anything should be changed

--- app/src/main/java/com/example/imadassessmentmarch30/MainActivity.kt
+++ app/src/main/java/com/example/imadassessmentmarch30/MainActivity.kt
@@ -6,8 +6,7 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +14,42 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+//on my layout page I have to finish labeling and adding more to the interface
 
         /*Declarations of variable for my app. this is the 1st major step I have taken into building the
 application for IMAD5112 assignment */
 
-//on my layout page I have to finish labeling and adding more to the interface
 
         val enterTimeOfDayButton = findViewById<Button>(R.id.btnEnterTimeOfDay)
         val resetButton = findViewById<Button>(R.id.ResetBtn)
         val theSuggestion = findViewById<TextView>(R.id.SuggestingText)
         val inputText = findViewById<EditText>(R.id.InputText)
 
+        //this button will be used when entering the time of day on the app
+
         enterTimeOfDayButton.setOnClickListener {
-            val input = enterTimeOfDayButton.text.toString()
+            val input = enterTimeOfDayButton.text.toString().trim()
+
+            //these are the social suggestions
+
+            val message = when (input){
+                "morning"->"Lets try make someone laugh today"
+                "afternoon"->"like the late queen lets have tea and crumpets"
+                "evening"->"Maybe its time to go for a walk"
+                "night"->"Gotta make sure you've told your friends about your day"
+                "midnight"->"Nearly bed time call your closest friend and say goodnight"
+                else -> "Invalid Time of Day"
+            }
+
+            theSuggestion.text=message
+
+        }
+        //the reset button will reset the input field and the suggestions field
+
+        resetButton.setOnClickListener {
+            inputText.text.clear()
+            theSuggestion.text="Suggestions Show here"
+
         }
 
         }
